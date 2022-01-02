6a088393f2fdcc65d33991fb29252b56c9e6b007
ewized <ewized@gmail.com>
2022-01-02 22:21:08 +0000

    Unit test for generate_world switch to constant generics

--- gameoflife/src/main.rs
+++ gameoflife/src/main.rs
@@ -2,24 +2,23 @@ use std::fmt::{Display, Formatter, Result};
 use std::io::{stdin, stdout, Write};
 use std::process;
 
-struct Point {
-  x: usize,
-  y: usize,
-}
-
-const SIZE: Point = Point { x: 80, y: 15 };
+const WIDTH: usize = 80;
+const HEIGHT: usize = 15;
 const BLOCK: char = '*';
 const SPACE: char = ' ';
 
-type PlayingArea = [[char; SIZE.x]; SIZE.y];
+type PlayingArea<const X: usize, const Y: usize> = [[char; X]; Y];
 
 /// Generates the world's PlayingArea while giving a closure that decides what to set each cell char
 /// The cell_generation has an argument of a tuple (x, y) that is the point that represents the cell
-fn generate_world<F: Fn((usize, usize)) -> bool>(cell_generation: F) -> PlayingArea {
-  let mut world = [[SPACE; SIZE.x]; SIZE.y];
+fn generate_world<F, const X: usize, const Y: usize>(cell_generation: F) -> PlayingArea<X, Y>
+where
+  F: Fn((usize, usize)) -> bool,
+{
+  let mut world = [[SPACE; X]; Y];
 
-  for x in 0..SIZE.x {
-    for y in 0..SIZE.y {
+  for x in 0..X {
+    for y in 0..Y {
       world[y][x] = if cell_generation((x, y)) {
         BLOCK
       } else {
@@ -32,7 +31,11 @@ fn generate_world<F: Fn((usize, usize)) -> bool>(cell_generation: F) -> PlayingA
 }
 
 /// Count the cells around origin cell and return if it should be alive or dead
-fn count_neighbors(world: PlayingArea, x: usize, y: usize) -> u8 {
+fn count_neighbors<const X: usize, const Y: usize>(
+  world: PlayingArea<X, Y>,
+  x: usize,
+  y: usize,
+) -> u8 {
   let mut count = 0;
 
   for i in -1..=1 {
@@ -41,8 +44,8 @@ fn count_neighbors(world: PlayingArea, x: usize, y: usize) -> u8 {
       let rel_y = y as isize + j;
 
       // make sure the value can fit in the PlayingArea and is not the origin
-      if (rel_x >= 0 && rel_x < SIZE.x as isize)
-        && (rel_y >= 0 && rel_y < SIZE.y as isize)
+      if (rel_x >= 0 && rel_x < X as isize)
+        && (rel_y >= 0 && rel_y < Y as isize)
         && !(rel_x == x as isize && rel_y == y as isize)
         && world[rel_y as usize][rel_x as usize] == BLOCK
       {
@@ -55,7 +58,7 @@ fn count_neighbors(world: PlayingArea, x: usize, y: usize) -> u8 {
 }
 
 struct World {
-  world: PlayingArea,
+  world: PlayingArea<HEIGHT, WIDTH>,
 }
 
 impl World {
@@ -119,16 +122,36 @@ mod tests {
   use super::*;
 
   #[test]
+  fn world_generation() {
+    let empty_world = [
+      [SPACE, SPACE, SPACE, SPACE, SPACE],
+      [SPACE, SPACE, SPACE, SPACE, SPACE],
+      [SPACE, SPACE, SPACE, SPACE, SPACE],
+      [SPACE, SPACE, SPACE, SPACE, SPACE],
+      [SPACE, SPACE, SPACE, SPACE, SPACE],
+    ];
+    let filled_world = [
+      [BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
+      [BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
+      [BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
+      [BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
+      [BLOCK, BLOCK, BLOCK, BLOCK, BLOCK],
+    ];
+    assert_eq!(generate_world::<_, 5, 5>(|_| false), empty_world);
+    assert_eq!(generate_world::<_, 5, 5>(|_| true), filled_world);
+  }
+
+  #[test]
   fn number_of_neighbors() {
-    let empty_world = generate_world(|_| false);
-    let filled_world = generate_world(|_| true);
+    let empty_world = generate_world::<_, WIDTH, HEIGHT>(|_| false);
+    let filled_world = generate_world::<_, WIDTH, HEIGHT>(|_| true);
 
     assert_eq!(count_neighbors(empty_world, 0, 0), 0);
     assert_eq!(count_neighbors(empty_world, 1, 1), 0);
-    assert_eq!(count_neighbors(empty_world, SIZE.x - 1, SIZE.y - 1), 0);
+    assert_eq!(count_neighbors(empty_world, WIDTH - 1, HEIGHT - 1), 0);
 
     assert_eq!(count_neighbors(filled_world, 0, 0), 3);
-    assert_eq!(count_neighbors(filled_world, SIZE.x - 1, SIZE.y - 1), 3);
+    assert_eq!(count_neighbors(filled_world, WIDTH - 1, HEIGHT - 1), 3);
     assert_eq!(count_neighbors(filled_world, 1, 1), 8);
   }
 }
